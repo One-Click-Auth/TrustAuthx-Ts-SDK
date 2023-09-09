48388499c554b25bd9478ac0cbb99c30d1be3ba4
moonlightnexus <68702919+moonlightnexus@users.noreply.github.com>
2023-09-09 22:00:08 +0530

    Update README.ts

--- README.ts
+++ README.ts
@@ -1,161 +1,220 @@
-import * as requests from 'requests';
-import { HTTPError } from 'requests.exceptions';
-import { JWTError, jwt } from 'jose';
-import { ALGORITHMS } from 'jose.constants';
-import * as json from 'json';
+import { decode, encode, JWTError } from 'jsonwebtoken';
+import { ALGORITHMS } from 'jsonwebtoken';
 
 class AuthLiteClient {
-    secret_key: string;
-    api_key: string;
-    org_id: string | null;
-    signed_key: string;
-    jwt_encode: (key: string, data: object) => string;
-    jwt_decode: (key: string, data: string) => object;
+  private secretKey: string;
+  private apiKey: string;
+  private orgId?: string;
+  private signedKey: string;
 
-    class TokenCheck {
-        access: string;
-        refresh: string;
-        state: boolean;
+  constructor(apiKey: string, secretKey: string, orgId?: string) {
+    this.secretKey = secretKey;
+    this.apiKey = apiKey;
+    this.orgId = orgId;
+    this.signedKey = this.jwtEncode(secretKey, { api_key: apiKey });
+  }
+
+  private jwtEncode(key: string, data: object): string {
+    return encode(data, key, { algorithm: ALGORITHMS.HS256 });
+  }
+
+  private jwtDecode(key: string, data: string): object {
+    return decode(data, key, { algorithms: [ALGORITHMS.HS256] }) as object;
+  }
+
+  generateUrl(): string {
+    if (this.orgId) {
+      return `https://app.trustauthx.com/widget/login/?org_id=${this.orgId}`;
+    } else {
+      throw new Error('Must provide org_id');
+    }
+  }
+
+  generateEditUserUrl(accessToken: string, url: string): string {
+    const headers = { accept: 'application/json' };
+    const params = new URLSearchParams({
+      AccessToken: accessToken,
+      api_key: this.apiKey,
+      signed_key: this.signedKey,
+      url: url,
+    });
+
+    return fetch('https://api.trustauthx.com/api/user/me/settings/?' + params.toString(), {
+      method: 'GET',
+      headers: headers,
+    }).then((response) => {
+      return response.url;
+    });
+  }
+
+  async reAuth(code: string): Promise<object> {
+    const url = 'https://api.trustauthx.com/api/user/me/widget/re-auth/token';
+    const params = new URLSearchParams({
+      code: code,
+      api_key: this.apiKey,
+      signed_key: this.signedKey,
+    });
+    const headers = { accept: 'application/json' };
+
+    try {
+      const response = await fetch(url + '?' + params.toString(), {
+        method: 'GET',
+        headers: headers,
+      });
+
+      if (response.status === 200) {
+        const data = await response.json();
+        const rtn = this.jwtDecode(this.secretKey, JSON.stringify(data));
+        const sub = JSON.parse(rtn['sub']);
+        delete rtn['sub'];
+        rtn['email'] = sub['email'];
+        rtn['uid'] = sub['uid'];
+        return rtn;
+      } else {
+        throw new Error(`Request failed with status code: ${response.status}\n${await response.text()}`);
+      }
+    } catch (error) {
+      throw new Error(`Request failed: ${error.message}`);
+    }
+  }
+
+  async getUser(token: string): Promise<object> {
+    const url = 'https://api.trustauthx.com/api/user/me/auth/data';
+    const params = new URLSearchParams({
+      UserToken: token,
+      api_key: this.apiKey,
+      signed_key: this.signedKey,
+    });
+    const headers = { accept: 'application/json' };
+
+    try {
+      const response = await fetch(url + '?' + params.toString(), {
+        method: 'GET',
+        headers: headers,
+      });
+
+      if (response.status === 200) {
+        const data = await response.json();
+        const rtn = this.jwtDecode(this.secretKey, JSON.stringify(data));
+        const sub = JSON.parse(rtn['sub']);
+        delete rtn['sub'];
+        rtn['email'] = sub['email'];
+        rtn['uid'] = sub['uid'];
+        return rtn;
+      } else {
+        throw new Error(`Request failed with status code: ${response.status}\n${await response.text()}`);
+      }
+    } catch (error) {
+      throw new Error(`Request failed: ${error.message}`);
+    }
+  }
+
+  async getAccessTokenFromRefreshToken(refreshToken: string): Promise<object> {
+    const url = 'https://api.trustauthx.com/api/user/me/access/token/';
+    const params = new URLSearchParams({
+      RefreshToken: refreshToken,
+      api_key: this.apiKey,
+      signed_key: this.signedKey,
+    });
+    const headers = { accept: 'application/json' };
+
+    try {
+      const response = await fetch(url + '?' + params.toString(), {
+        method: 'GET',
+        headers: headers,
+      });
+
+      if (response.status === 200) {
+        return await response.json();
+      } else {
+        throw new Error(`Request failed with status code: ${response.status}\n${await response.text()}`);
+      }
+    } catch (error) {
+      throw new Error(`Request failed: ${error.message}`);
+    }
+  }
+
+  async validateAccessToken(access_token: string): Promise<boolean> {
+    const url = 'https://api.trustauthx.com/api/user/me/auth/validate/token';
+    const params = new URLSearchParams({
+      AccessToken: access_token,
+      api_key: this.apiKey,
+      signed_key: this.signedKey,
+    });
+    const headers = { accept: 'application/json' };
+
+    try {
+      const response = await fetch(url + '?' + params.toString(), {
+        method: 'GET',
+        headers: headers,
+      });
+
+      return response.status === 200;
+    } catch (error) {
+      throw new Error(`Request failed: ${error.message}`);
+    }
+  }
+
+  async revokeToken(AccessToken: string | null = null, RefreshToken: string | null = null, revokeAllTokens: boolean = false): Promise<boolean> {
+    const url = 'https://api.trustauthx.com/api/user/me/token/';
+    const headers = { accept: 'application/json' };
+
+    if (!AccessToken && !RefreshToken) {
+      throw new Error('Must provide either AccessToken or RefreshToken');
     }
 
-    constructor(api_key: string, secret_key: string, org_id?: string) {
-        this.jwt_encode = (key, data) => jwt.encode(data, key = key, algorithm = ALGORITHMS.HS256);
-        this.jwt_decode = (key, data) => jwt.decode(String(data), key = key, algorithms = ALGORITHMS.HS256);
-        this.secret_key = secret_key;
-        this.api_key = api_key;
-        this.org_id = org_id || null;
-        this.signed_key = this.jwt_encode(key = this.secret_key, data = { "api_key": this.api_key });
-    }
+    const tt = !!AccessToken;
+    const t = AccessToken || RefreshToken;
+    const params = new URLSearchParams({
+      Token: t,
+      api_key: this.apiKey,
+      signed_key: this.signedKey,
+      AccessToken: tt.toString(),
+      SpecificTokenOnly: (!revokeAllTokens).toString(),
+    });
 
-    generate_url(): string {
-        if (this.org_id) return `https://app.trustauthx.com/widget/login/?org_id=${this.org_id}`;
-        else throw new Error("must provide org_id");
-    }
+    try {
+      const response = await fetch(url + '?' + params.toString(), {
+        method: 'DELETE',
+        headers: headers,
+      });
 
-    generate_edit_user_url(access_token: string, url: string): string {
-        let headers = { 'accept': 'application/json' };
-        let params = {
-            'AccessToken': access_token,
-            'api_key': this.api_key,
-            'signed_key': this.signed_key,
-            'url': url
-        };
-        let url_ = "https://api.trustauthx.com/api/user/me/settings/";
-        let req = requests.Request('GET', url_, params = params, headers = headers).prepare();
-        return req.url;
+      return response.status === 200;
+    } catch (error) {
+      throw new Error(`Request failed: ${error.message}`);
     }
+  }
 
-    re_auth(code: string): object {
-        let url_ = "https://api.trustauthx.com/api/user/me/widget/re-auth/token";
-        let params = {
-            "code": code,
-            'api_key': this.api_key,
-            'signed_key': this.signed_key
-        };
-        let headers = { "accept": "application/json" };
-        let response = requests.get(url_, headers = headers, params = params);
-        if (response.status_code == 200) {
-            let rtn = this.jwt_decode(this.secret_key, response.json());
-            let sub = json.loads(rtn["sub"]);
-            rtn.pop("sub");
-            rtn["email"] = sub["email"];
-            rtn["uid"] = sub["uid"];
-            return rtn;
+  async validateTokenSet(access_token: string, refresh_token: string): Promise<TokenCheck> {
+    try {
+      const d: TokenCheck = {
+        access: '',
+        refresh: '',
+        state: false,
+      };
+      const is_valid = await this.validateAccessToken(access_token);
+      if (!is_valid) {
+        if (refresh_token) {
+          const new_tokens = await this.getAccessTokenFromRefreshToken(refresh_token);
+          d.state = false;
+          d.access = new_tokens['access_token'];
+          d.refresh = new_tokens['refresh_token'];
         }
-        else throw new HTTPError(
-            `Request failed with status code : ${response.status_code} \n this code contains a msg : ${response.text}`
-        );
+        return d;
+      } else {
+        d.state = true;
+        d.access = access_token;
+        d.refresh = refresh_token;
+        return d;
+      }
+    } catch (error) {
+      throw new Error('Both tokens are invalid, please log in again');
     }
+  }
+}
 
-    get_user(token: string): object {
-        let url_ = 'https://api.trustauthx.com/api/user/me/auth/data';
-        let headers = { 'accept': 'application/json' };
-        let params = {
-            'UserToken': token,
-            'api_key': this.api_key,
-            'signed_key': this.signed_key
-        };
-        let response = requests.get(url_, headers=headers, params=params);
-        if (response.status_code == 200) {
-            let rtn = this.jwt_decode(this.secret_key,response.json());
-            let sub = json.loads(rtn["sub"]);
-            rtn.pop("sub");
-            rtn["email"] = sub["email"];
-            rtn["uid"] = sub["uid"];
-            return rtn;
-        }
-        else throw new HTTPError(
-            `Request failed with status code : ${response.status_code} \n this code contains a msg : ${response.text}`
-        );
-    }
-
-    get_access_token_from_refresh_token(refresh_token: string): object {
-        let url_ = 'https://api.trustauthx.com/api/user/me/access/token/';
-        let headers = { 'accept': 'application/json' };
-        let params = {
-            'RefreshToken': refresh_token,
-            'api_key': this.api_key,
-            'signed_key': this.signed_key
-                 };
-        let response = requests.get(url_, headers = headers, params = params);
-        if (response.status_code == 200) return response.json();
-        else throw new HTTPError(
-            `Request failed with status code : ${response.status_code} \n this code contains a msg : ${response.text}`
-        );
-    }
-
-    validate_access_token(access_token: string): boolean {
-        let url_ = 'https://api.trustauthx.com/api/user/me/auth/validate/token';
-        let headers = { 'accept': 'application/json' };
-        let params = {
-            'AccessToken': access_token,
-            'api_key': this.api_key,
-            'signed_key': this.signed_key
-        };
-        let response = requests.get(url_, headers = headers, params = params);
-        return response.status_code == 200;
-    }
-
-    revoke_token(AccessToken?: string, RefreshToken?: string, revoke_all_tokens: boolean = false): object {
-        let url_ = 'https://api.trustauthx.com/api/user/me/token/';
-        let headers = { 'accept': 'application/json' };
-        if (!AccessToken && !RefreshToken) throw new Error("must provide either AccessToken or RefreshToken");
-        let tt = true if AccessToken else false;
-        let t = AccessToken || RefreshToken;
-        let params = {
-            'Token': t,
-            'api_key': this.api_key,
-            'signed_key': this.signed_key,
-            'AccessToken': tt,
-            'SpecificTokenOnly': !revoke_all_tokens,
-        };
-        let response = requests.delete(url_, headers = headers, params = params);
-        if (response.status_code == 200) return response.json();
-        else throw new HTTPError(
-            `Request failed with status code : ${response.status_code} \n this code contains a msg : ${response.text}`
-        );
-    }
-
-    validate_token_set(access_token: string, refresh_token: string): TokenCheck {
-        try {
-            let d = new this.TokenCheck();
-            let is_valid = this.validate_access_token(access_token);
-            if (!is_valid) {
-                if (refresh_token) {
-                    let new_tokens = this.get_access_token_from_refresh_token(refresh_token);
-                    d.state = false;
-                    d.access = new_tokens['access_token'];
-                    d.refresh = new_tokens['refresh_token'];
-                }
-                return d;
-            } else {
-                d.state = true;
-                d.access = access_token;
-                d.refresh = refresh_token;
-                return d;
-            }
-        } catch (e) {
-            throw new HTTPError('both tokens are invalid login again');
-        }
-    }
+class TokenCheck {
+  access: string;
+  refresh: string;
+  state: boolean;
 }
